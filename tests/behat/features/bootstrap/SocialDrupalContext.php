0b226c38919f95d94bbd641a8e785428cc9367e0
navneet0693 <navneetsingh0693@gmail.com>
2022-11-07 08:02:12 +0530

    Update inline variable description.

--- tests/behat/features/bootstrap/SocialDrupalContext.php
+++ tests/behat/features/bootstrap/SocialDrupalContext.php
@@ -278,12 +278,12 @@ class SocialDrupalContext extends DrupalContext {
    */
   protected function processQueue($just_delete = FALSE) {
     $workerManager = \Drupal::service('plugin.manager.queue_worker');
-    /** @var Drupal\Core\Queue\QueueFactory; $queue */
+    /** @var \Drupal\Core\Queue\QueueFactory; $queue */
     $queue = \Drupal::service('queue');
 
     for ($i = 0; $i < 20; $i++) {
       foreach ($workerManager->getDefinitions() as $name => $info) {
-        /** @var Drupal\Core\Queue\QueueInterface $worker */
+        /** @var \Drupal\Core\Queue\QueueInterface $worker */
         $worker = $queue->get($name);
 
         /** @var \Drupal\Core\Queue\QueueWorkerInterface $queue_worker */
