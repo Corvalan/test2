navneet0693/open_social

Fix views access check due to parameters not upcasting
Apparently parameters in routes on views are not upcast. This is tracked
by https://www.drupal.org/project/drupal/issues/2528166

In “Replace blocked users route checks with entity checks” the
requirements for some views on user profiles were changed to use entity
access checks. However, the entity access checks had no user to check
against.

The dependency on social_core is added to social_user.info.yml to ensure
the verified role is installed before we try to assign permissions.
Additionally social_user was actually using some things from social_core
which would also require social_core to be installed.

modules/social_features/social_user/src/Plugin/views/access/PeopleAccess.php
@@ -7,7 +7,7 @@ use Symfony\Component\Routing\Route;
 use Drupal\views\Plugin\views\access\AccessPluginBase;
 
 /**
- * People page access plugin that provides access control based on some perms.
+ * People page access plugin that provides access control for admin overviews.
  *
  * @ingroup views_access_plugins
  *
@@ -30,17 +30,15 @@ class PeopleAccess extends AccessPluginBase {
    * {@inheritdoc}
    */
   public function access(AccountInterface $account) {
-    // Check if user has administer users or view user access.
-    $administerUsers = $account->hasPermission('administer users');
-    $viewUsers = $account->hasPermission('view users');
-    return $administerUsers | $viewUsers;
+    return $account->hasPermission("access administration pages")
+      && $account->hasPermission("list users");
   }
 
   /**
    * {@inheritdoc}
    */
   public function alterRouteDefinition(Route $route) {
-    $route->setRequirement('_custom_access', '\Drupal\social_user\Controller\SocialUserController::access');
+    $route->setRequirement('_permission', 'access administration pages,list users');
   }
 
 }

Corvalan/test2

modules/social_features/social_user/src/Plugin/views/access/PeopleAccess.php
@@ -7,7 +7,7 @@ use Symfony\Component\Routing\Route;
 use Drupal\views\Plugin\views\access\AccessPluginBase;
 
 /**
- * People page access plugin that provides access control based on some perms.
+ * People page access plugin that provides access control for admin overviews.
  *
  * @ingroup views_access_plugins
  *
@@ -30,17 +30,15 @@ class PeopleAccess extends AccessPluginBase {
    * {@inheritdoc}
    */
   public function access(AccountInterface $account) {
-    // Check if user has administer users or view user access.
-    $administerUsers = $account->hasPermission('administer users');
-    $viewUsers = $account->hasPermission('view users');
-    return $administerUsers | $viewUsers;
+    return $account->hasPermission("access administration pages")
+      && $account->hasPermission("list user");
   }
 
   /**
    * {@inheritdoc}
    */
   public function alterRouteDefinition(Route $route) {
-    $route->setRequirement('_custom_access', '\Drupal\social_user\Controller\SocialUserController::access');
+    $route->setRequirement('_permission', 'access administration pages,list user');
   }
 
 }